2e36009eb3ef15f0820676c4e5a35bc3db555773
Marina <mpyak93@gmail.com>
2021-12-12 19:25:35 -0500

    Added a new component to show synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
@@ -7,9 +8,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
-              <em>{definition.example}</em>
+              <em>
+                <strong>Example:</strong>
+                {definition.example}
+              </em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
